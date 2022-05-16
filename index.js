916b3be5ae5c505d6800ed3fbbb850d5d99d9cd0
Your Name <ustinovoleksij@gmail.com>
2022-05-16 15:36:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const person = {
+    name: "Alex",
+    age: 25,
+
+    get userAge() {
+        return this.age;
+    },
+
+    set userAge(num) {
+        this.age = num;
+    }
+};
+
+console.log(person.userAge = 30);
+console.log(person.userAge);
